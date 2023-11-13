cf84d5538285db37e2cc93972efff2fadac1f2bf
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2023-11-13 11:07:58 +0600

    images added yet set

---
+++ src/components/Category.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Category = () => {
+    
+    return (
+        <div className="flex flex-col">
+            <img src={img} alt="" />
+            
+        </div>
+    );
+};
+
+export default Category;
--- src/components/DealsDetail.jsx
+++ src/components/DealsDetail.jsx
@@ -53,24 +53,6 @@ const DealsDetail = () => {
           </div>
         </div>
       </article>
-
-      <img src="https://i.ibb.co/TT3rxZq/fish-1.png" alt="fish-1" border="0" />
-      <img
-        src="https://i.ibb.co/FKpCk7Z/vegetables.png"
-        alt="vegetables"
-        border="0"
-      />
-      <img src="https://i.ibb.co/KKPqTF1/milk.png" alt="milk" border="0" />
-      <img src="https://i.ibb.co/98T7NSM/meat.png" alt="meat" border="0" />
-      <img src="https://i.ibb.co/y80tfRY/fruits.png" alt="fruits" border="0" />
-      <img src="https://i.ibb.co/4S8LwKt/fish.png" alt="fish" border="0" />
-      <img src="https://i.ibb.co/nzFTRg7/soda.png" alt="soda" border="0" />
-      <img
-        src="https://i.ibb.co/KLmWyPP/cake-slice.png"
-        alt="cake-slice"
-        border="0"
-      />
-      <img src="https://i.ibb.co/h13WKSP/bread.png" alt="bread" border="0" />
     </div>
   );
 };
