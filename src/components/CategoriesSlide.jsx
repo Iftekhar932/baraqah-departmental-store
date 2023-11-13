873d3725488b381524be14e888f5affce6c93004
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2023-11-13 18:50:21 +0600

    slide design added

--- src/components/CategoriesSlide.jsx
+++ src/components/CategoriesSlide.jsx
@@ -1,16 +1,4 @@
 import React, { useState } from "react";
-/* 
-const imgs = [
-  "https://i.ibb.co/TT3rxZq/fish-1.png",
-  "https://i.ibb.co/FKpCk7Z/vegetables.png",
-  "https://i.ibb.co/KKPqTF1/milk.png",
-  "https://i.ibb.co/98T7NSM/meat.png",
-  "https://i.ibb.co/y80tfRY/fruits.png",
-  "https://i.ibb.co/4S8LwKt/fish.png",
-  "https://i.ibb.co/nzFTRg7/soda.png",
-  "https://i.ibb.co/KLmWyPP/cake-slice.png",
-  "https://i.ibb.co/h13WKSP/bread.png",
-]; */
 
 const CategoriesSlide = () => {
   const imgs = [
@@ -26,49 +14,57 @@ const CategoriesSlide = () => {
   ];
   const [current, setCurrent] = useState(2);
 
+  // makes sure if it is the last item then it doesn't go more that that and goes back to the first item again, e.g: items.length =8, it won't let index become 9
   const nextSlide = () => {
     setCurrent(current === imgs.length - 1 ? 0 : current + 1);
   };
 
+  // makes sure if it is the first item it doesn't go to negative
   const prevSlide = () => {
     setCurrent(current === 0 ? imgs.length - 1 : current - 1);
   };
 
   return (
-    <div className="flex items-center justify-center">
-      <button onClick={prevSlide} className="btn btn-primary mr-4">
-        Prev
-      </button>
-      <div className="flex space-x-4">
-        <img
-          src={imgs[(current - 2 + imgs.length) % imgs.length]}
-          alt=""
-          className="w-16 h-16 md:w-32 md:h-32 object-cover rounded-lg"
-        />
-        <img
-          src={imgs[(current - 1 + imgs.length) % imgs.length]}
-          alt=""
-          className="w-16 h-16 md:w-32 md:h-32 object-cover rounded-lg"
-        />
-        <img
-          src={imgs[current]}
-          alt=""
-          className="w-16 h-16 md:w-32 md:h-32 object-cover rounded-lg"
-        />
-        <img
-          src={imgs[(current + 1) % imgs.length]}
-          alt=""
-          className="w-16 h-16 md:w-32 md:h-32 object-cover rounded-lg"
-        />
-        <img
-          src={imgs[(current + 2) % imgs.length]}
-          alt=""
-          className="w-16 h-16 md:w-32 md:h-32 object-cover rounded-lg"
-        />
+    <div>
+      <div className="flex justify-between w-3/4 mx-auto">
+        <span className="font-bold">Categories</span>
+        <span className="text-green-700 text-lg">View All</span>
       </div>
-      <button onClick={nextSlide} className="btn btn-primary ml-4">
-        Next
-      </button>
+      <div className="flex items-center justify-center">
+        <button onClick={prevSlide} className="btn btn-primary mr-4">
+          Prev
+        </button>
+        <div className="flex space-x-4">
+          <img
+            src={imgs[(current - 2 + imgs.length) % imgs.length]}
+            alt=""
+            className="w-16 h-16 sm:w-32 sm:h-32 object-cover rounded-lg"
+          />
+          <img
+            src={imgs[(current - 1 + imgs.length) % imgs.length]}
+            alt=""
+            className="w-16 h-16 sm:w-32 sm:h-32 object-cover rounded-lg"
+          />
+          <img
+            src={imgs[current]}
+            alt=""
+            className="w-16 h-16 sm:w-32 sm:h-32 object-cover rounded-lg"
+          />
+          <img
+            src={imgs[(current + 1) % imgs.length]}
+            alt=""
+            className="w-16 h-16 sm:w-32 sm:h-32 object-cover rounded-lg"
+          />
+          <img
+            src={imgs[(current + 2) % imgs.length]}
+            alt=""
+            className="w-16 h-16 sm:w-32 sm:h-32 object-cover rounded-lg"
+          />
+        </div>
+        <button onClick={nextSlide} className="btn btn-primary ml-4">
+          Next
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -1,9 +1,41 @@
 import React from "react";
 
+const themeNames = [
+  "light",
+  "dark",
+  "cupcake",
+  "bumblebee",
+  "emerald",
+  "corporate",
+  "synthwave",
+  "retro",
+  "cyberpunk",
+  "valentine",
+  "halloween",
+  "garden",
+  "forest",
+  "aqua",
+  "lofi",
+  "pastel",
+  "fantasy",
+  "wireframe",
+  "black",
+  "luxury",
+  "dracula",
+  "cmyk",
+  "autumn",
+  "business",
+  "acid",
+  "lemonade",
+  "night",
+  "coffee",
+  "winter",
+];
+
 const ThemeSwitcher = () => {
   return (
     <div className="join join-vertical">
-      <input
+      {/* <input
         type="radio"
         name="theme-buttons"
         className="btn theme-controller join-item"
@@ -37,7 +69,18 @@ const ThemeSwitcher = () => {
         className="btn theme-controller join-item"
         aria-label="Aqua"
         value="aqua"
-      />
+      /> */}
+      {themeNames.map((themeName) => {
+        return (
+          <input
+            type="radio"
+            name="theme-buttons"
+            className="btn theme-controller join-item"
+            aria-label={`${themeName}`}
+            value={`${themeName}`}
+          />
+        );
+      })}
     </div>
   );
 };
