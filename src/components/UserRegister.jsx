43f42e0a443a0601366b759dfe1f14cd15d15a86
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2023-11-18 22:22:32 +0600

    conflit solved

--- src/components/UserRegister.jsx
+++ src/components/UserRegister.jsx
@@ -15,7 +15,7 @@ const UserRegister = () => {
     setUserPassword(password);
   };
 
-  const submitFunction = (e, flag) => {
+  const submitFunction = (flag) => {
     e.preventDefault();
     if (flag == "google") {
       signInWithGoogle();
@@ -85,7 +85,7 @@ const UserRegister = () => {
             </div>
             <button
               className="btn btn-primary"
-              onClick={(e) => submitFunction(e, "google")}
+              onClick={(e) => submitFunction("google")}
             >
               Sign In with google
             </button>
