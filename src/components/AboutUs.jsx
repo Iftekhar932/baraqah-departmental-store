11a436eb9e72bbc68e4c406e80b2a991d603d5fc
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2023-11-29 19:11:59 +0600

    cartview table added

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const AboutUs = () => {
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="max-w-7xl mx-auto px-4 pb-12 sm:px-6 lg:px-8">
       <div className="p-8 shadow-md rounded-lg">
         <h1 className="text-3xl font-bold text-green-700">
           Baraqah: Your Gateway to a Healthier, Happier You
         </h1>
-        <p className="text-lg text-orange-600 mt-4">
+        <p className="text-lg font-semibold mt-4">
           At Baraqah, we believe in the power of wholesome, organic food to
           nourish your body and transform your well-being. We are committed to
           providing you with the finest selection of 100% organic and fresh
@@ -20,7 +20,7 @@ const AboutUs = () => {
         <h2 className="text-2xl font-bold text-green-700">
           Embracing a Healthier Lifestyle
         </h2>
-        <p className="text-lg text-orange-600 mt-4">
+        <p className="text-lg font-semibold mt-4">
           Our journey began with a simple passion for healthy living. We
           recognized the profound impact of food on our overall health,
           vitality, and energy levels. Inspired by this realization, we embarked
@@ -33,7 +33,7 @@ const AboutUs = () => {
         <h2 className="text-2xl font-bold text-green-700">
           Our Commitment to Quality
         </h2>
-        <p className="text-lg text-orange-600 mt-4">
+        <p className="text-lg font-semibold mt-4">
           We are dedicated to sourcing the highest quality organic produce from
           trusted farmers and suppliers around the globe. Our stringent
           standards ensure that our products meet the strictest organic
@@ -46,7 +46,7 @@ const AboutUs = () => {
         <h2 className="text-2xl font-bold text-green-700">
           A Culinary Adventure
         </h2>
-        <p className="text-lg text-orange-600 mt-4">
+        <p className="text-lg font-semibold mt-4">
           Our online store is a haven for those seeking a culinary adventure
           that nourishes both body and soul. Explore our diverse range of
           organic products, from fresh fruits and vegetables to pantry staples,
@@ -58,7 +58,7 @@ const AboutUs = () => {
         <h2 className="text-2xl font-bold text-green-700">
           Your Trusted Partner in Wellness
         </h2>
-        <p className="text-lg text-orange-600 mt-4">
+        <p className="text-lg font-semibold mt-4">
           We believe in fostering a community of health-conscious individuals
           who share our passion for organic living. Our team of experts is
           always here to guide you through your journey, providing personalized
@@ -67,7 +67,7 @@ const AboutUs = () => {
       </div>
 
       <div className="mt-8 text-center">
-        <p className="text-lg text-orange-600">
+        <p className="text-lg font-semibold">
           Join us on the path to a healthier, happier you. Embrace the goodness
           of nature and experience the transformative power of Baraqah's organic
           and fresh products. Together, let's nourish our bodies, nurture our
--- src/components/CartView.jsx
+++ src/components/CartView.jsx
@@ -1,63 +1,76 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 const CartView = () => {
-  let products = JSON.parse(localStorage?.getItem("userProducts"));
+  const [cartProductDisplay, setCartProductDisplay] = useState([]);
 
-  products.map((product) => {
-    const response = axios
-      .get(`http://localhost:3001/getAllProducts/${product.productId}`, {
-        withCredentials: true,
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-        },
-      })
-      .then((response) => {
-        const { _id, category, categoryImg, id, img, name, price, unit } =
-          response.data[0];
-        console.log(response.data[0]);
-      })
-      .catch((err) => console.log(err, "line 21 product.js"));
+  useEffect(() => {
+    if (!localStorage.getItem("userProducts")) return;
+    const products = JSON.parse(localStorage.getItem("userProducts"));
 
-    return response;
-  });
+    const fetchCartProducts = async () => {
+      const productRequests = products.map((product) => {
+        return axios
+          .get(`http://localhost:3001/getAllProducts/${product.productId}`, {
+            withCredentials: true,
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+            },
+          })
+          .then((response) => {
+            const { _id, category, categoryImg, id, img, name, price, unit } =
+              response.data[0];
 
-  let cartProductsDisplay = (
-    <>
-      <tr>
-        <td>{category}</td>
-        <td>{name}</td>
-        <td>{price}</td>
-        <td>{unit}</td>
-      </tr>
-    </>
-  );
+            return {
+              id,
+              name,
+              price,
+              unit,
+              category,
+              categoryImg,
+              img,
+              qnt: product.qnt,
+            };
+          })
+          .catch((err) => console.log(err, "line 21 product.js"));
+      });
+
+      const cartProducts = await Promise.all(productRequests); // axios api call function makes multiple asynchronous calls so used "Promise.all()" to wait for it to finish
+      setCartProductDisplay(cartProducts);
+    };
+
+    fetchCartProducts();
+  }, []);
+
+  if (!cartProductDisplay) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <div className="overflow-x-auto">
+    <div className="overflow-x-auto mx-auto mt-4 w-4/5 p-4 border">
       <table className="table table-xs">
         <thead>
           <tr>
-            <th></th>
-            <th>Name</th>
-            <th>Job</th>
-            <th>company</th>
-            <th>location</th>
-            <th>Last Login</th>
-            <th>Favorite Color</th>
+            <th>Index</th>
+            <th>Category</th>
+            <th>Product Name</th>
+            <th>Price</th>
+            <th>Unit</th>
+            <th>Quantity</th>
           </tr>
         </thead>
-        {/* <tbody>{productsToDisplay}</tbody> */}
-        <tfoot>
-          <tr>
-            <th></th>
-            <th>Name</th>
-            <th>Job</th>
-            <th>company</th>
-            <th>location</th>
-            <th>Last Login</th>
-            <th>Favorite Color</th>
-          </tr>
-        </tfoot>
+        <tbody>
+          {cartProductDisplay.map((product, index) => (
+            <tr key={product.id}>
+              <td className="border">{index + 1}</td>
+              <td className="border">{product.category}</td>
+              <td className="border">{product.name}</td>
+              <td className="border">{product.price}</td>
+              <td className="border">{product.unit}</td>
+              <td className="border">{product.qnt}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
