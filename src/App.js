47e83b9aca1265b2688140c1df4523463710ed4f
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2023-12-21 11:15:47 +0600

    added comments

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import router from "./Routes/routes";
 ! add deletion function for admin (TEST WITH GOOGLE ACCOUNTS)
 ! add refreshToken feature for google
 ! add footer
+! need to add more json data to mongodb
 */
 
 function App() {
