77425769320fdf4dc17845567dfc2a67d5de7275
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2024-01-15 14:40:37 +0600

    reduced code that was commented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/routes";
 
-/* 
-! user self account deletion feature 
-! try adding blob download feature
-*/
-
 function App() {
   return <RouterProvider router={router}></RouterProvider>;
 }
