1cc22464608edf9d933156376bab323cb8d7604e
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2025-05-18 20:04:43 +0600

    fixed loadingSpinner component

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LoadingSpinner = ({ loading }: { loading: boolean }) => {
   return (
-    <div className={`text-center my-4 ${loading ? "hidden" : ""}`}>
+    <div className={`text-center my-4 ${loading == false ? "hidden" : ""}`}>
       <span className="text-purple-400 loading loading-ring loading-lg"></span>
       <span className="text-purple-800 loading loading-ring loading-lg"></span>
       <span className="text-purple-600 loading loading-ring loading-lg"></span>
