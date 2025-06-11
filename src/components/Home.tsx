9f8382b6986ee6158043a0d3b73d7c7c92eeaf89
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2025-06-11 09:29:20 +0600

    added useeffect for testing in hom

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,6 +4,16 @@ import DealsDetail from "./DealsDetail";
 import Hero from "./Hero";
 
 const Home = () => {
+  React.useEffect(() => {
+    console.log("Home component mounted");
+    fetch(
+      "https://baraqah-departmental-store-server.onrender.com/getAllProducts"
+    )
+      .then((response) => response.json())
+      .catch((error) => {
+        console.error("Error fetching products:", error);
+      });
+  }, []);
   return (
     <>
       <Hero />
