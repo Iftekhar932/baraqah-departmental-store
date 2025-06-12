78b7bea4ba92597cd0b4c90afce702d24c0738ff
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2025-06-12 23:06:45 +0600

    removed useeffect from home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,16 +2,8 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import DealsDetail from "./DealsDetail";
 import Hero from "./Hero";
-import { JWTExpiryHandlerFunction } from "../Routes/routes";
 
 const Home = () => {
-  React.useEffect(() => {
-    console.log("Home component mounted");
-    JWTExpiryHandlerFunction(
-      "https://baraqah-departmental-store-server.onrender.com/getAllProducts",
-      "SliderCategory.jsx €- API getAllProducts"
-    );
-  }, []);
   return (
     <>
       <Hero />
